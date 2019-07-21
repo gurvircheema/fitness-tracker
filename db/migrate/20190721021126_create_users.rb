848d2ddf6041c2dbc00f4e06af49a26a5dd41c82
Gurvir Cheema <gurvircheema@hotmail.com>
2019-07-20 19:46:35 -0700

    Add user model

---
+++ db/migrate/20190721021126_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :age
+      t.integer :height_cm
+      t.integer :weight_lbs
+      t.string :email
+      t.string :phone
+      t.string :time_zone
+
+      t.timestamps
+    end
+  end
+end
