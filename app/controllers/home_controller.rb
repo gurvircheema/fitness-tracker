41213a9f8c5f2f8dcb39ae0aebb8ff6b71aea291
Gurvir Cheema <gurvircheema@hotmail.com>
2019-07-28 13:54:26 -0700

    Add layouts for guest and devise with rails notification

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
+  layout :resolve_layout
+
   def index
+  end
+
+  private
+
+  def resolve_layout
+    if user_signed_in?
+      "application"
+    else
+      "guest"
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def flash_message
+    flash.map do |key, msg|
+      content_tag :div, id: key, class: flash_class(key) do
+        content_tag :button, class: 'delete'
+        msg
+      end
+    end.join.html_safe
+  end
+
+  private
+
+  def flash_class(key)
+    class_string = case key
+    when 'alert'
+      'is-danger'
+    else
+      'is-info'
+    end
+    "notification is-marginless is-radiusless #{class_string}"
+  end
 end
